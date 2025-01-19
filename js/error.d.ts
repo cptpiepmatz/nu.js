72b08d887650d9c0db2dc451ec4f9d72267d8ea7
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2025-01-19 15:08:48 +0100

    exclude generated js files

--- js/error.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class TryFromValueError extends Error {
-    readonly value: object;
-    constructor(message: string, value: object);
-}
--- js/error.js
+++
@@ -1,7 +0,0 @@
-export class TryFromValueError extends Error {
-    value;
-    constructor(message, value) {
-        super(message);
-        this.value = value;
-    }
-}
